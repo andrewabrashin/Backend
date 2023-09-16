d2e6104b52c40409abe83a6d221c183561308d8a
abrashin_andrej <andrewabrashin@gmail.com>
2023-09-16 20:54:06 +0300

    task 2.2

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+const getAPI = async () => {
+	const res = await fetch('https://jsonplaceholder.typicode.com/todos/1/');
+
+	if (res.ok) {
+		const data = await res.json();
+		console.log(data);
+	}
+
+};
+
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hello World');
+
+});
+server.listen(3000, () => {
+	console.log('Server running at http://127.0.0.1:3000/');
+});
