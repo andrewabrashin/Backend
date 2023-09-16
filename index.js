590bab22e15337640f3e1f87a8a0afd17ffe75f0
abrashin_andrej <andrewabrashin@gmail.com>
2023-09-16 21:16:15 +0300

    task 2.2

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const getAPI = async () => {
-	const res = await fetch('https://jsonplaceholder.typicode.com/todos/1/');
+	const https = require('https');
+	const options = {
+		hostname: 'jsonplaceholder.typicode.com',
+		path: '/todos/1',
+		method: 'GET',
+	};
+	// создаем запрос
 
-	if (res.ok) {
-		const data = await res.json();
-		console.log(data);
-	}
-
+	const request = https.request(options, (res) => {
+		console.log(`statusCode: ${res.statusCode}`)
+		res.on('data', (d) => {
+			// вывод полученных в ответе данных
+			process.stdout.write(d)
+			const fs = require('fs');
+			fs.writeFileSync('test.txt', d);
+		})
+	});
+	request.on('error', (error) => {
+		console.error(error)
+	});
+	request.end();
 };
 
 const http = require('http');
@@ -13,7 +27,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Hello World');
-
+	getAPI();
 });
 server.listen(3000, () => {
 	console.log('Server running at http://127.0.0.1:3000/');
